5ee3bd2978f0e6847ba6d6e78de5d547ead98937
CelicMilos <165153757+CelicMilos@users.noreply.github.com>
2024-04-17 16:33:33 +0200

    Add files via upload

---
+++ OOP/app.js
@@ -0,0 +1,61 @@
+// CONSTRUCTOR FUNCTION - they generate objects.
+// Praksa je da imena CF-a pocinju velikim slovom.
+
+function Todo(name, completed) {
+  this.name = name;
+  this.completed = completed;
+}
+
+const todo = new Todo("Buy Aplles", false);
+const todo2 = new Todo("Buy bannanas", true);
+const todo3 = new Todo("Buy a book", true);
+
+console.log(todo);
+console.log(todo2);
+console.log(todo3);
+
+// CF with methods
+
+function Books(title, author) {
+  this.title = title;
+  this.author = author;
+  this.getAuthor = function () {
+    console.log(`The author of the book '${this.title}' is ${this.author}.`);
+  };
+}
+
+const book1 = new Books("Homo Deus", "Juval Noa Harari");
+const book2 = new Books("First three minutes", "Steven Veinberg");
+const book3 = new Books("Great Silence", "Milan M. Cirkovic");
+const book4 = new Books("Breef History of time", "Steven Hoking");
+
+console.log(book2);
+console.log(book4);
+book3.getAuthor();
+book1.getAuthor();
+
+// Prototypes  -  svi objekti imaju prototipove.
+// Prototipovi su vec ugradjeni metodi (npr za Arr .push  .slice ...).
+// Mi mozemo sami da pravimo svoje prototipove.
+// Ukoliko CF ima metode tj. funkcije,svakim nivim pozivanjem CF-a pravi se i mesto za
+// metod pa tako moze da dodje do povecanja memorije.
+// Taj problem se resava prototipovima.
+
+function NewBooks(title, author) {
+  this.title = title;
+  this.author = author;
+}
+
+NewBooks.prototype.getAuthor = function () {
+  console.log(`The author of the book '${this.title}' is ${this.author}.`);
+};
+
+const book5 = new NewBooks("Decoding Reality", "Vlatko Vedral");
+const book6 = new NewBooks("Tuesdays with Morrie", "Mitch Elbom");
+const book7 = new NewBooks("48 Laws of Power", "Robert Green");
+const book8 = new NewBooks("On The Beach", "Nevile Shute");
+
+console.log(book6);
+console.log(book8);
+book7.getAuthor();
+book5.getAuthor();
